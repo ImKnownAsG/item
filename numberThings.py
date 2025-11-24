85d8697ccc0cd63b68598edc50079da04c9558b6
ImKnownAsG <chris.gieselman@gmail.com>
2025-11-23 21:38:27 -0500

    A working (-ish) sqrt() method and a ceil() method

---
+++ numberThings.py
@@ -0,0 +1,39 @@
+from numbers import Num
+
+absVar = -5
+#print(f'abs(absVal): {Num.abs(absVar)}')
+
+powVal = 5
+powExp = 2
+#print(f'{powVal}^{powExp}: {Num.pow(powVal, powExp)}')
+
+sqrtVal = 10.89
+print(f'sqrt({sqrtVal}): {Num.sqrt(sqrtVal)}')
+print(f'bin(33): {bin(33)}')
+
+#print(f'bin(3.3): {bit(3.3)}')
+
+import struct
+
+def float_to_binary(num):
+    # Pack the float into a 4-byte (32-bit) binary string in big-endian format
+    # '!' for network byte order (big-endian), 'f' for float
+    packed = struct.pack('!f', num)
+    
+    # Unpack the binary string as an unsigned integer
+    # 'I' for unsigned int
+    integer_representation = struct.unpack('!I', packed)[0]
+    
+    # Format the integer as a 32-bit binary string with leading zeros
+    binary_string = f"{integer_representation:032b}"
+    
+    return binary_string
+
+# Example usage:
+my_float = 3.3
+binary_repr = float_to_binary(my_float)
+print(f"The binary representation of {my_float} is  : {binary_repr}")
+
+my_float_negative = 10.89
+binary_repr_neg = float_to_binary(my_float_negative)
+print(f"The binary representation of {my_float_negative} is: {binary_repr_neg}")
--- numbers.py
+++ numbers.py
@@ -1,4 +1,6 @@
 class Num:
+    _precision = .00001
+        
     def abs(x):
         result = x
         if type(x) == str:
@@ -14,13 +16,112 @@ class Num:
                 print(f'something bad happened')
                 result = 0
         else:
-            result = 'Not a valid type'
+            raise TypeError('Not a valid type')
+        return result
+    def pow(b, e):
+        result = 1
+        if e != 0:
+            result = b
+            count = 1
+            while count < Num.abs(e):
+                result = result * b 
+                count = count + 1
+        if e < 0:
+            result = 1 / result
+        
+        return result
+    
+    def ceil(x):
+        return int(x) + 1
+
+    def sqrt_joe(x):
+        if x > 0:
+            guess = x/2.0
+            diff = x - Num.pow(guess, 2.0)
+            while Num.abs(diff) > Num._precision:
+                if diff < 0:
+                    guess = guess/2.0
+                else:
+                    guess = guess * 1.5
+                diff = x - pow(guess, 2.0)
+        elif x == 0:
+            guess = 0
+        else:
+            raise 'complex numbers not implemented yet'
+        
+        return guess
+        
+    def sqrt(x):
+        if x < 0:
+            if x == 0:
+                guess == 0
+            else:
+                raise ValueError('Complex numbers not implemented yet')
+        else:
+            #establish initial high and low guesses, start guessing with the high value
+            for _ in range(Num.ceil(x)):
+                if pow(_, 2) < x:
+                    lowGuess = _
+                else:
+                    highGuess = _
+                    break
+            
+            guess = highGuess 
+            
+            #check how close our current guess is and guess again if it is too far off
+            while abs(pow(guess, 2) - x) > Num._precision:
+                #if current guess is too high
+                if Num.pow(guess, 2) > x:
+                    #new guess is halfway between current guess and lowGguess
+                    guess = lowGuess + ((guess - lowGuess) * .5)
+                    
+                else:
+                    guess = highGuess - ((highGuess - guess) * .5)
+                
+                #if the new guess is still too high set it to highGuess to define a new upper bound
+                if Num.pow(guess, 2) > x:
+                    highGuess = guess
+                #otherwise use it as the new lower bound
+                else:
+                    lowGuess = guess
+            
+            #the logic above can create a very ugly decimal, much of which is unnecessary
+            #starting from the integer value and adding decimal places, find the value of
+            #guess with the least decimal places that satisfies the _precision requirement
+            refGuess = int(guess)
+            #as a way of truncating the decimals, guess is multiplied by a power of 10,
+            #the int value is returned, then that int is divided by the same power of 10
+            thisPow = 0 
+            
+            while abs(pow(refGuess, 2) - x) > Num._precision:
+                #refPlus and refMin are attempts to get around the floating point inaccuracies...a failed attempt
+                refPlus = int((refGuess + Num._precision) * Num.pow(10, thisPow)) / Num.pow(10, thisPow)
+                refMin = int((refGuess - Num._precision) * Num.pow(10, thisPow)) / Num.pow(10, thisPow)
+                print(f'refPlus, refMin: {refPlus}, {refMin}')
+                print(f'Num.pow(refPlus, 2), Num.pow(refMin, 2): {Num.pow(refPlus, 2)}, {Num.pow(refMin, 2)}')
+                if Num.pow(refPlus, 2) == x:
+                    refGuess = refPlus
+                elif Num.pow(refMin, 2) == x:
+                    refGuess = refMin
+                else:
+                    refGuess = int(guess * Num.pow(10, thisPow)) / Num.pow(10, thisPow)
+                    thisPow += 1
+                    
+        print(f'guess, refGuess: {guess}, {refGuess}')
+        return refGuess
+                
+    def pi():
+        sign = -1
+        deno = 3
+        result = 1
+        while 1/deno > Num._precision:
+            result += sign/denom
+            sign *= -1
+            denom += 2
+        result *= 4
         return result
 
 class Complex:
     real = 0
     imag = 0
 
-y = '0'
-    
-print(f'Num.abs({y}): {Num.abs(y)}')
